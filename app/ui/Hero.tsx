e7b0417e5577a866565db46a9dfbf0e045fb511e
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-08-19 14:29:01 -0300

    feature: create hero section with nav and header

---
+++ app/ui/Hero.tsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Nav from "./Nav";
+
+export default function Hero(){
+
+    return (
+        <div className="relative" >
+
+            <Nav />
+
+            <Header />
+
+        </div>
+    )
+
+}
