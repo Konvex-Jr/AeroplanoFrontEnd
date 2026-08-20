7d72fe25252e2c80908d30c793e8915692e5069c
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-08-20 01:40:41 -0300

    fix: updating alt texts and fixing icons width and height

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -20,6 +20,8 @@ export default function Footer({ className }: FooterProps){
     const width = useWindowWidth()
 
     if(width === 0) return
+
+    const iconSize = width < 800 ? 20 : 30
     
     return (
         <div className={`${className} bg-background-footer text-white font-bold`}>
@@ -27,32 +29,32 @@ export default function Footer({ className }: FooterProps){
             {/* Contacts */}
             <div className="flex flex-col gap-6 text-center w-full py-8 md:ml-20 lg:ml-32" >
                 <div className="flex gap-2" >
-                    <Image src={map_icon.src} width={width < 800 ? 20 : 30} height={width < 800 ? 20 : 30} alt="Ícone de Pino de Mapa" />
+                    <Image src={map_icon.src} width={0} height={0} style={{ width: iconSize, height: iconSize }} alt="Ícone de Pino de Mapa" />
                     <div className="text-start text-[2cqw] md:text-lg" >
                         <p>Rua Doutor João Colin, 1285, Sala 3</p>
                         <p>Joinville SC Brasil</p>
                     </div>
                 </div>
                 <div className="flex gap-4" >
-                    <Image src={phone_icon.src} width={width < 800 ? 20 : 30} height={width < 800 ? 20 : 30} alt="Ícone de Pino de Mapa" />
+                    <Image src={phone_icon.src} width={0} height={0} style={{ width: iconSize, height: iconSize }} alt="Ícone de Telefone" />
                     <div className="text-start text-[2cqw] md:text-lg" >
                         <p>(47) 99962-6217</p>
                     </div>
                 </div>
                 <div className="flex gap-4" >
-                    <Image src={phone_icon.src} width={width < 800 ? 20 : 30} height={width < 800 ? 20 : 30} alt="Ícone de Pino de Mapa" />
+                    <Image src={phone_icon.src} width={0} height={0} style={{ width: iconSize, height: iconSize }} alt="Ícone de Telefone" />
                     <div className="text-start text-[2cqw] md:text-lg" >
                         <p>(47) 99108-5092</p>
                     </div>
                 </div>
                 <div className="flex gap-4" >
-                    <Image src={instagram_icon.src} width={width < 800 ? 20 : 30} height={width < 800 ? 20 : 30} alt="Ícone de Pino de Mapa" />
+                    <Image src={instagram_icon.src} width={0} height={0} style={{ width: iconSize, height: iconSize }} alt="Ícone do Instagram" />
                     <div className="text-start text-[2cqw] md:text-lg" >
                         <p>aeroplano_design</p>
                     </div>
                 </div>
                 <div className="flex gap-4" >
-                    <Image src={email_icon.src} width={width < 800 ? 20 : 30} height={width < 800 ? 20 : 30} alt="Ícone de Pino de Mapa" />
+                    <Image src={email_icon.src} width={0} height={0} style={{ width: iconSize, height: iconSize }} alt="Ícone de Email" />
                     <div className="text-start text-[2cqw] md:text-lg" >
                         <p>Contato@aeroplano.com.br</p>
                     </div>
