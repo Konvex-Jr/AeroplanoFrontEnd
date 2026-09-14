20d484f7b962f90746290732b1de2b0e4d01df41
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-09-14 16:40:52 -0300

    feature: integration with resend template done

--- app/api/resend/route.ts
+++ app/api/resend/route.ts
@@ -1,9 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
+import { sendEmailBodySchema } from "../schemas";
+import { CreateEmailResponse, Resend } from 'resend';
+import { config } from "dotenv";
+
+config()
 
 // Handler da ResendAPI.
 
-export default async function POST(req: NextRequest){
+export async function POST(request: NextRequest, reponse: NextResponse){
+
+    const parsed = sendEmailBodySchema.safeParse(await request.json())
+
+    if(!parsed.success) return NextResponse.json({ message: "Validation Error." }, { status: 422 })
+
+    const { name, email, need  } = parsed.data
+
+    const emailToSend = 'exemplo@email.com'
+
+    // Logs da Requisição
+    console.log("Name : ", name);
+    console.log("Email: ", email);
+    console.log("Need : ", need);
 
     // Realiza uma REQUISIÇÃO para a API da Resend
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    const res: CreateEmailResponse = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: [ emailToSend ],
+        subject: `Novo Email de ${name}`,
+        html: (`
+            <p>Remetente: <strong>${email}</strong></p>
+            <p>Necessidade: <strong>${need}</strong></p>
+        `)
+    });
+
+    if(res.error) return NextResponse.json({ message: "Algo deu errado," }, { status: 500 })
+
+    return NextResponse.json({ message: "Email enviado com sucesso." }, { status: 200 })
 
 }
