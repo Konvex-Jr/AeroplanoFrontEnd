9342a690c9820a3cbdc61273af6a7445af7198ef
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-09-14 15:41:37 -0300

    fix: delete unused files

--- app/api/submit.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default async function handler(req: NextRequest, res: NextResponse){
-
-    // Realiza uma REQUISIÇÃO para a API da Resend
-
-}
