d99253839cd08daf27de8fafc8249dbedc357028
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2026-09-14 23:09:32 -0300

    feature: login page styles modified

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,11 @@ import Nav from "@/app/ui/Nav";
 
 export default function Page(){
     return (
-        <div>
+        <div className="flex flex-col items-center" >
             
             <Nav />
+
+            <h1 className="w-full text-center text-2xl lg:text-4xl font-bold pb-8" >Login</h1>
 
             <LoginForm />
 
--- app/ui/LoginForm.tsx
+++ app/ui/LoginForm.tsx
@@ -28,21 +28,23 @@ export default function LoginForm(){
     }
 
     return (
-        <form className="flex flex-col items-center justify-center w-full gap-8 text-[3cqw] md:text-lg" onSubmit={(e) => { onSubmit(e) }} >
+        <div className="flex w-[80%] lg:w-[50%] h-[50%] border-2 rounded-2xl py-8" >
+            <form className="flex flex-col items-center justify-center w-full gap-8 text-[3cqw] md:text-lg mb-10" onSubmit={(e) => { onSubmit(e) }} >
 
-            <div className="flex flex-col gap-1 px-6 w-full lg:w-[40%]">
-                <label className="text-start" htmlFor="email">Email</label>
-                <input className="bg-white border-0 p-2 text-black font-normal outline-0 w-full" type="email" name="email" id="email" required />
-            </div>
+                <div className="flex flex-col gap-1 px-6 w-full lg:w-[80%]">
+                    <label className="text-start" htmlFor="email">Email</label>
+                    <input className="bg-white border-0 p-2 text-black font-normal outline-0 w-full" type="email" name="email" id="email" required />
+                </div>
 
-            <div className="flex flex-col items-start gap-1 px-6 w-full lg:w-[40%]">
-                <label htmlFor="name">Senha</label>
-                <input className="bg-white border-0 p-2 text-black font-normal outline-0 w-full" type="password" name="password" id="password" required />
-            </div>
+                <div className="flex flex-col items-start gap-1 px-6 w-full lg:w-[80%]">
+                    <label htmlFor="name">Senha</label>
+                    <input className="bg-white border-0 p-2 text-black font-normal outline-0 w-full" type="password" name="password" id="password" required />
+                </div>
 
-            <button className="flex justify-center border-2 border-white hover:bg-gray-600 w-[20%] px-2 cursor-pointer" type="submit" >Login</button>
+                <button className="flex justify-center border-2 border-white hover:bg-gray-400 w-[20%] px-2 py-1 cursor-pointer" type="submit" >Login</button>
 
-        </form>
+            </form>
+        </div>
     )
 
 }
