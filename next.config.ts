12d5fc2f7ccbf5add17035c7791f46318d6a8fdb
GabrielVinicius1106 <gabrielviniciusdacruz546@gmail.com>
2026-09-24 21:19:24 +0000

    fix: remove conflict markers from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  output: "standalone",
-
-=======
   output: 'standalone',
->>>>>>> 8044324 (fix: add standalone to next build config)
   images: {
     qualities: [75, 100],
   },
